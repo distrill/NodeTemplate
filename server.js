a116f58b473d84bed4f8198b9bcf1ddf7dfd152b
Brent Hamilton <brent@arthurkn.net>
2016-10-05 09:32:11 -0700

    user can specify HOST environment variable

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const express = require('./config/express');
 const app = express();
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || '127.0.0.1';
 
-app.listen(port, '127.0.0.1');
+app.listen(port, host);
 
-console.log('temp app running at http://localhost:' + port + '/');
+console.log('temp app running at http://' + host + ':' + port + '/');
